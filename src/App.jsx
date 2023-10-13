b5fb170e4e558e3fc2908a92f796a899f4863063
Phoulos <sghaier.hedi@live.fr>
2023-10-13 11:24:58 +0200

    Pokédex - Passer des props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+  },
+];
+
+
 function components() {
   return (
     <div>
-      <PokemonCard />
+      {pokemonList.map((pokemon) => <PokemonCard key={pokemon.name} pokemon={pokemon} />)}
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
 
-const PokemonCard = () => {
+const PokemonCard = ({ pokemon }) => {
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            <img src={pokemon.imgSrc} alt="Bulbasaur" />
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
 
 export default PokemonCard;
 
-
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
+/* 
 function PokemonCard2() {
     const pokemon = pokemonList[0];
     return (
         // Utilisez les données de pokemon pour afficher les informations dynamiquement
         <div>
             <h2>{pokemon.name}</h2>
-            {/* ... Autres informations ... */}
         </div>
     );
 }
 
+function PokemonCard(props) {
+    console.log(props);
+    // Reste du code du composant
+}
+ */
